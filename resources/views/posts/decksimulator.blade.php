0f8a734fe8eb491e64f31b67381a6633c5933929
goma_chan <ryo.fith@gmail.com>
2025-06-15 23:31:21 +0900

    バリ島最終報告7

--- resources/views/posts/decksimulator.blade.php
+++ resources/views/posts/decksimulator.blade.php
@@ -141,8 +141,9 @@
                                             <div class="card-item-area-inner">
                                                 <div class="card-item-element">
                                                     <figure>
-                                                        <img src="{{ $card->image_url }}" alt="{{ $card->name }}"
-                                                            class="img-fluid" />
+                                                        <img src="{{ asset('images/' . $card->class_en . '/' . $card->image_url) }}"
+                                                            alt="{{ $card->name }}" class="img-fluid" />
+
                                                     </figure>
                                                     <div class="mt-2">
                                                         {{-- 任意の詳細表示 --}}
